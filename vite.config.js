edd8424e767bdf7a7edccd8289c96815310040ef
Haneen <7anoon.987@gmail.com>
2026-01-23 23:31:23 +0200

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/bekya/', // غيري "bekya" لاسم الـ repository بتاعك
+  base: '/bekya2.1/',
   plugins: [
     react(),
     VitePWA({
